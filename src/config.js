0972ff0600b74e52c655024a211585feb0d9acf9
Maryam Bagheri <80166076+maryambagheri1989@users.noreply.github.com>
2022-03-12 23:08:59 +0330

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,93 +1,77 @@
 // config.js
 module.exports = {
     github: {
-        username: 'arifszn', // Your GitHub org/user name. (Required)
-        sortBy: 'stars', // stars | updated
-        limit: 8, // How many projects to display.
+        username: 'maryambagheri1989', // Your GitHub org/user name. (Required)
+        sortBy: 'updated', // stars | updated
+        limit: 4, // How many projects to display.
         exclude: {
-            forks: false, // Forked projects will not be displayed if set to true.
+            forks: true, // Forked projects will not be displayed if set to true.
             projects: ['laravel-ecommerce'] // These projects will not be displayed. example: ['my-project1', 'my-project2']
         }
     },
     social: {
-        linkedin: 'ariful-alam',
-        twitter: 'arif_swozon',
+        linkedin: 'maryam-bagheri-15623450',
+        twitter: '',
         facebook: '',
         dribbble: '',
         behance: '',
         medium: '',
-        devto: 'arifszn',
-        website: 'https://arifszn.github.io',
+        devto: '',
+        website: 'https://maryambagheri1989.github.io',
         phone: '',
-        email: 'arifulalamszn@gmail.com'
+        email: ''
     },
     skills: [
-        'PHP',
-        'Laravel',
-        'JavaScript',
-        'React.js',
-        'Node.js',
-        'MySQL',
-        'Git',
-        'Docker',
-        'CSS',
-        'Antd',
-        'Tailwind',
-        'Bootstrap',
+        'Java',
+        'Research'
     ],
     experiences: [
         { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Backend Engineer II',
-            from: 'September 2021',
+            company: 'Soon',
+            position: '...',
+            from: '...',
             to: 'Present'
         },
         { 
-            company: 'Orangetoolz',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
-            to: 'August 2021'
-        },
-        { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
+            company: 'Soon',
+            position: '...',
+            from: '...',
+            to: '...'
         }
     ],
     education: [
         { 
-            institution: 'American International University-Bangladesh',
-            degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
+            institution: 'Sharif',
+            degree: '...',
+            from: '...',
+            to: '...'
         },
         { 
-            institution: 'Cantonment College, Jessore',
-            degree: 'Higher Secondary Certificate (HSC)',
-            from: '2012',
-            to: '2014',
+            institution: 'Sharif',
+            degree: '...',
+            from: '...',
+            to: '...',
         },
         { 
-            institution: 'Chowgacha Shahadat Pilot High School',
-            degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
-            to: '2012'
+            institution: 'Sharif',
+            degree: '...',
+            from: '...',
+            to: '...'
         }
     ],
     blog: {
         // Display blog posts from your medium or dev.to account. (Optional)
-        source: 'dev.to', // medium | dev.to
-        username: 'arifszn',
-        limit: 3 // How many posts to display. Max is 10.
+        //source: 'dev.to', // medium | dev.to
+       // username: 'arifszn',
+       // limit: 3 // How many posts to display. Max is 10.
     },
     googleAnalytics: {
         // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-        id: 'G-WLLB5E14M6' // Please remove this and use your own tag id or keep it empty
+       // id: 'G-WLLB5E14M6' // Please remove this and use your own tag id or keep it empty
     },
     hotjar: {
-        id: '2617601', //  Please remove this and use your own id or keep it empty
-        snippetVersion : 6
+       // id: '2617601', //  Please remove this and use your own id or keep it empty
+       // snippetVersion : 6
     },
     themeConfig: {
         default: 'light',
